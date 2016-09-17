52a768e51e15c1598ad95e49f325795fc5a8ac39
Chris White <coldliquidflame@gmail.com>
2016-09-17 04:25:08 +0000

    Removed commented code

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ var express = require('express'),
     app = express(),
     apiRoutes = require('./routes/api');
 
-// Initialize and setup pins
-/*wpi.wiringPiSetupGpio();
-wpi.pinMode(green, wpi.OUTPUT);
-wpi.pinMode(red, wpi.OUTPUT);
-
-wpi.pinMode(button, wpi.INPUT);
-wpi.pullUpDnControl(button, wpi.PUD_UP);
-wpi.wiringPiISR(button, wpi.INT_EDGE_FALLING, function(delta) {
-	console.log('Button was pressed. (' + delta + ')');
-});
-*/
-// Setup routes
-
 app.use('/api', apiRoutes);
 app.use(express.static('public'));
 
@@ -22,11 +9,3 @@ app.use(express.static('public'));
 app.listen(3000, function() {
 	console.log('LED app listening on 3000');
 });
-
-// Functions
-/*function turnAllOff() {
-	wpi.digitalWrite(red, wpi.LOW);
-	wpi.digitalWrite(green, wpi.LOW);
-}*/
-
-// Events
